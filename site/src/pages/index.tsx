c92c14e14ed0a04b82f66df44dcec3d7faca7e3c
caleb <caleb.peterson@cubicle6.com>
2023-06-28 17:26:41 -0400

    cleanup unused importsin pages/index.tsx

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,19 +1,11 @@
-import { readdir } from "node:fs/promises";
-import path from "node:path";
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { getGuidelinesRoot } from "@/utils/getGuidelinesRoot";
 import Link from "next/link";
 import { TopNav } from "@/components/TopNav";
-import { startCase } from "lodash";
 import { getTitleFromSlug } from "@/utils/getTitleFromSlug";
 import { Layout } from "@/components/Layout";
 import { Footer } from "@/components/Footer";
 import { getGuidelinesSlugs } from "@/utils/getGuidelinesSlugs";
-
-const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
   slugs: string[];
